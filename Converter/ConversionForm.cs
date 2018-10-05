209cf340a92ab5bb4ad1e334f37b5805d444ec3f
Adomix <keithaaronr1@gmail.com>
2018-10-05 18:20:07 -0400

    Initial Commit

---
+++ Converter/ConversionForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class ConversionForm : Form
+    {
+        public decimal BaseValue { get; set; }
+        WARequest Request { get; set; }
+        Point PointA { get; set; }
+        Point PointB { get; set; }
+
+        public ConversionForm()
+        {
+            InitializeComponent();
+            BaseValue = 0;
+            Request = new WARequest();
+            PointA = new Point(146, 71);
+            PointB = new Point(312, 71);
+        }
+
+        private void InputBox_TextChanged(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(InputBox.Text, out var result))
+            {
+                BaseValue = result;
+            }
+            else
+            {
+                InputBox.Undo();
+            }
+        }
+
+        private async void ConvertButton_Click(object sender, EventArgs e)
+        {
+            ComboBox[] boxes = new ComboBox[] { MetricUnits, ImperialUnits };
+            var unitA = boxes.FirstOrDefault(x => x.Location == PointA).SelectedItem.ToString();
+            var unitB = boxes.FirstOrDefault(x => x.Location == PointB).SelectedItem.ToString();
+
+            if (Resultant.Text != string.Empty)
+            {
+                Resultant.Text = string.Empty;
+            }
+            Resultant.Text = "Loading!";
+            var result = await Request.WaAsync(BaseValue, unitA, unitB);
+            Resultant.Text = result;
+        }
+
+        private void UnitButton_Click(object sender, EventArgs e)
+        {
+            if (UnitButton.Text.Contains("Metric"))
+            {
+                UnitButton.Text = "To Imperial";
+                MetricUnits.Location = PointA;
+                ImperialUnits.Location = PointB;
+            }
+            else
+            {
+                UnitButton.Text = "To Metric";
+                MetricUnits.Location = PointB;
+                ImperialUnits.Location = PointA;
+            }
+        }
+    }
+}
---
+++ Converter/WARequest.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Converter
+{
+    public class WARequest
+    {
+        private readonly HttpClient _http;
+
+        public WARequest()
+        {
+            _http = new HttpClient();
+        }
+
+        public async Task<string> WaAsync(decimal valueA, string unitA, string unitB)
+        {
+            using (var req = await _http.GetAsync($"http://api.wolframalpha.com/v1/result?appid={ConfigurationManager.AppSettings["wolfram"]}&i={valueA.ToString()}{unitA} to {unitB}"))
+            {
+                var content = await req.Content.ReadAsStringAsync();
+
+                if (content.Contains("understand"))
+                {
+                    content = "Request could not be completed!";
+                }
+
+                return content;
+            }
+        }
+
+    }
+}
